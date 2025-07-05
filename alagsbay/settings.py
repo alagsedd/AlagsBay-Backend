bfb8881353c09de98d18524142419dba71e6f530
Edmund Algpulinsa <alagsedd@gmail.com>
2025-07-05 23:27:37 +0000

    Initial commit: Django backend and React frontend with MySQL dump

--- alagsbay/settings.py
+++ alagsbay/settings.py
@@ -96,26 +96,26 @@ WSGI_APPLICATION = 'alagsbay.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'alagsbay',
-#         'USER': 'root',
-#         'PASSWORD': 'Ed123456789{}',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'ZitFfyCuUyZokNazhEHWEqijWSFnDxBg',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
