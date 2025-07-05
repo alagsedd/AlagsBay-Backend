9e007d79eb45e2b04c16f6ebc5261cd63d5ef4d2
Edmund Algpulinsa <alagsedd@gmail.com>
2025-07-05 22:27:39 +0000

    Switch to SQLite for Render deployment

--- alagsbay/settings.py
+++ alagsbay/settings.py
@@ -96,19 +96,27 @@ WSGI_APPLICATION = 'alagsbay.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'alagsbay',
+#         'USER': 'root',
+#         'PASSWORD': 'Ed123456789{}',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'alagsbay',
-        'USER': 'root',
-        'PASSWORD': 'Ed123456789{}',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
